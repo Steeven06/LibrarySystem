e2c7d750f799038b5349ab16352dcac95d00d3b4
steeven06 steeven <steeven06jimenez@gmail.com>
2025-11-18 12:50:19 -0500

    Implementacion de servicios del autor

---
+++ LibrarySystem.Domain/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using LibrarySystem.Domain.Entities;
+
+namespace LibrarySystem.Domain.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        Task<List<Author>> GetAllAsync();
+        Task<Author?> GetByIdAsync(Guid id);
+        Task AddAsync(Author author);
+        Task UpdateAsync(Author author);
+        Task DeleteAsync(Guid id);
+    }
+}
